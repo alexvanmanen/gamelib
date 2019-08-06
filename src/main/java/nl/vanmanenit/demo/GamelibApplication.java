d6634ac443949ced3ed8e2c5c7cd23ac83a8a732
Alex van Manen <alexvanmanen@hotmail.com>
2019-08-06 22:33:19 +0200

    start project

---
+++ src/main/java/nl/vanmanenit/demo/GamelibApplication.java
@@ -0,0 +1,13 @@
+package nl.vanmanenit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamelibApplication.class, args);
+    }
+
+}
